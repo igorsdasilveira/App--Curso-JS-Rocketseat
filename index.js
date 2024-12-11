1480dd0112c0904728164996ae186a50e8c18ed4
igorsdasilveira <igorssilveira17@gmail.com>
2024-12-11 13:41:02 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -60,10 +60,27 @@ const metasRealizadas = async () => {
    }
 
    await select({
-    message: "Metas Realizadas",
+    message: "Metas Realizadas " + realizadas.length,
     choices: [...realizadas]
    })
 }
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0) {
+        console.log ("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
 
 const start = async () => {
    
@@ -84,6 +101,10 @@ const start = async () => {
                 value: "realizadas"
             },
             {
+                name: "Metas abertas",
+                value: "abertas"
+            },
+            {
                 name: "Sair",
                 value: "sair"
             }
@@ -101,6 +122,9 @@ const start = async () => {
             break
         case "realizadas":
             await metasRealizadas()
+            break
+        case "abertas":
+            await metasAbertas()
             break
         case "sair":
         console.log("Até a próxima!")
